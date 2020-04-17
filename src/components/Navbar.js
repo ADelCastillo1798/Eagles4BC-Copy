85bd262c1380f66c83c69d492bd3136c83a1bc4c
Adam Del Castillo <ADelCastillo@Adams-MacBook-Pro.local>
2020-04-17 11:40:50 -0400

    Basic NavBar layout

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,23 @@
-import react from 'react';
+import React from 'react';
 import css from './Navbar.module.css';
 import publicUrl from 'utils/publicUrl';
 
 function Navbar() {
     return (
-        "Navbar"
+        <nav className={css.navbar}>
+            <div className={css.navItem}>
+                "needs"
+            </div>
+            <div className={css.navItem}>
+                "offers"
+            </div>
+            <div className={css.navItem}>
+                "responses"
+            </div>
+            <div className={css.navItem}>
+                "profile"
+            </div>
+        </nav>
     )
 }
 
