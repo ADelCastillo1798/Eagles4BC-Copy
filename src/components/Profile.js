2eb934e6c6100a9262cec5d8d3a46882e0c9ef64
Adam Del Castillo <ADelCastillo@Adams-MacBook-Pro.local>
2020-04-17 18:58:08 -0400

    Profile work

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -1,15 +1,30 @@
 import React, { useContext } from 'react';
 import publicUrl from 'utils/publicUrl'
 import { StoreContext } from 'contexts/StoreContexts';
+import { Link, useParams } from 'react-router-dom';
+import css from 'components/Profile.module.css';
 
 function Profile(){
 
   const store = useContext(StoreContext)
+  console.log(store)
+
+  let {userId} = useParams();
+  if (!userId){
+    userId = store.currentUserId;
+  }
+
+  let user = store.users.find(u => u.id === userId);
+  console.log(user)
+  let userPosts = store.posts.filter(p => p.userId === userId);
+  console.log(userPosts)
 
 
-    return(
-      Profile 
-    );
+  return(
+    <header className={css.header}>
+
+    </header>
+  );
 }
 
 export default Profile;
