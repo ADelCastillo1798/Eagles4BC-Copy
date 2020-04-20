61cce8f4fc24bcbfd28c8839c2a0031a76bc53d6
Adam Del Castillo <ADelCastillo@Adams-MacBook-Pro.local>
2020-04-20 12:59:30 -0400

    NewPost

--- src/components/NewPost.js
+++ src/components/NewPost.js
@@ -1,8 +1,90 @@
-import React from 'react';
+import React , { useState, useContext } from 'react';
+import {StoreContext} from 'contexts/StoreContext';
+import css from 'components/NewPost.module.css';
+import { useHistory } from "react-router-dom";
 
 function NewPost(){
+    const history = useHistory();
+    const {addPost} = useContext(StoreContext);
+    const [title, setTitle] = useState('');
+    const [desc, setDesc] = useState('');
+    const [type, setType] = useState('');
+    const [tags, setTags] = useState(['']);
+    const [error, setError] = useState('');
+
+    function handleTitleChange(e){
+        setTitle(e.target.value);
+        console.log(title);
+    }
+    function handleTypeChange(e){
+        setType(e.target.value);
+        console.log(type);
+    }
+    function handleTagsChange(e){
+        let tagString = e.target.value;
+        let tagSplit = tagString.split(", ");
+        setTags(tagSplit);
+        console.log(tags)
+    }
+    function handleDescChange(e){
+        setDesc(e.target.value);
+        console.log(desc);
+    }
+    function handleSubmit(e){
+        e.preventDefault();
+        if (title === ''){
+          setError('Please enter a title!');
+          return;
+        }
+        if (type === ''){
+            setError('Please select a type!');
+            return;
+          }
+        if (desc === ''){
+        setError('Please enter a description!');
+        return;
+        }
+        setError('');
+    
+        //update store
+        addPost(title, type, desc, tags);
+        history.push('/');
+      }
+    function handleCancel(){
+        history.goBack();
+      }
+
     return(
-        NewPost
+        <div className={css.main}>
+            <div className={css.header}>
+                <span>Create New</span>
+            </div>
+            <div className={css.title}>
+                <textarea placeholder="Title…" value={title} row="1" onChange={handleTitleChange}/>
+            </div>
+            <div className={css.type}>
+                <label>
+                    <input type="radio" value="need" checked={type === "need"} onChange={handleTypeChange} /> Need
+                </label>
+                <label>
+                    <input type="radio" value="offer" checked={type === "offer"} onChange={handleTypeChange} /> Offer
+                </label>
+            </div>
+            <div className={css.tags}>
+                <textarea placeholder="Enter tags separated by ,'s" onChange={handleDescChange} />
+            </div>
+            <div className={css.desc}>
+                <textarea placeholder="Describe your post…" rows="10" onChange={handleDescChange} />
+            </div>
+           
+            <div className={css.actions}>
+                <button onClick={handleCancel}>Cancel</button>
+                <button onClick={handleSubmit}>Post!</button>
+            </div>
+            <div className={css.error}>
+                {error}
+            </div>
+        </div>
     );
 }
 
