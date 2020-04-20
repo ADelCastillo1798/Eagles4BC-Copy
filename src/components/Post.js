3fb7d4b06dd7b9d98fee4bb80e2faf59e7fd3066
Adam Del Castillo <ADelCastillo@Adams-MacBook-Pro.local>
2020-04-19 22:15:36 -0400

    Profile navigation from post

--- src/components/Post.js
+++ src/components/Post.js
@@ -1,22 +1,34 @@
 import React from 'react';
 import css from 'components/Post.module.css';
 import publicUrl from 'utils/publicUrl';
-
+import { Link } from 'react-router-dom';
 
 function Post(props){
     return (
         <article className={css.post}>
-            <header className={css.title}>
-                {props.post.title}
-            </header>
-            <section className={css.desc}>
-                {props.post.desc}
-            </section>
-            <section className={css.tags}>
-                {props.post.tags.map(tag =>
-                    <span>{tag}</span>
-                )}
-            </section>
+            <div className={css.top}>
+                <div className={css.author}>
+                    <Link to={`/profile/${props.post.userId}`}>
+                        <img src={publicUrl(props.post.userPhoto)} alt='User Pic' />
+                        <span>{props.post.userId}</span>
+                    </Link>
+                </div>
+                <div>
+                    <header className={css.title}>
+                        {props.post.title}
+                    </header>
+                    <section className={css.desc}>
+                        {props.post.desc}
+                    </section>
+                </div>
+            </div>
+            <div className={css.bottom}>
+                <section className={css.tags}>
+                    {props.post.tags.map(tag =>
+                        <span>{tag}</span>
+                    )}
+                </section>
+            </div>
         </article>
     );
 }
