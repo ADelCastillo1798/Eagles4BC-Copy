5ab9fd71d775ccff9561c96371015aacd94d40f0
Esther Lee <leedfy@bc.edu>
2020-04-26 18:40:31 -0500

    more posts

--- src/utils/initStore.js
+++ src/utils/initStore.js
@@ -73,7 +73,7 @@ export default {
             type: "offer",
             title: "Food and Support if you need it!",
             desc: "I've got a place off campus and am here for anyone that needs help. I'm a decent cook so I can whipe up a little something and am open to talk and provide support in this stressful time!",
-            tags: ["Food", "Mental Helath Support", "Off-Campus"],
+            tags: ["Food", "Mental Health Support", "Off-Campus"],
             datetime: "2020-02-06T22:45:28Z"
         },
         {
@@ -86,5 +86,55 @@ export default {
             tags: ["Food", "Lower"],
             datetime: "2020-02-06T22:45:28Z"
         },
+        {
+            id: "post-6",
+            userId: "esther",
+            userPhoto: "/assets/user5.png",
+            type: "need",
+            title: "Moving Help",
+            desc: "I am moving into a new apartment near campus and I would love any extra hands. I have 6 boxes and it is hard to move on my own.",
+            tags: ["Moving", "Off-Campus"],
+            datetime: "2020-02-06T22:45:28Z"
+        },
+        {
+            id: "post-7",
+            userId: "tommy",
+            userPhoto: "/assets/user4.png",
+            type: "offer",
+            title: "Ride to Costco",
+            desc: "I have a van on-campus that can fit 7 other people for anyone that wants to visit Costco with me this weekend!",
+            tags: ["Transportation", "Off-Campus"],
+            datetime: "2020-02-06T22:45:28Z"
+        },
+        {
+            id: "post-8",
+            userId: "adam",
+            userPhoto: "/assets/user3.png",
+            type: "need",
+            title: "Laptop Charger",
+            desc: "My laptop charger just broke and the library ran out of chargers too. Does anyone have a spare USB-C cable they can lend me for a few days while I order a new one?",
+            tags: ["Electronics", "Computer"],
+            datetime: "2020-02-06T22:45:28Z"
+        },
+        {
+            id: "post-9",
+            userId: "esther",
+            userPhoto: "/assets/user5.png",
+            type: "offer",
+            title: "LinkedIn Photos",
+            desc: "I'm taking some LinkedIn photos for my friends, but I decided to extend my offer to anyone who needs one. I will by Stokes around noon for 3 hours on Satuday if anyone wants their picture taken! ",
+            tags: ["Photography", "On-Campus"],
+            datetime: "2020-02-06T22:45:28Z"
+        },
+        {
+            id: "post-10",
+            userId: "iggy",
+            userPhoto: "/assets/user2.png",
+            type: "need",
+            title: "Handsanitizer",
+            desc: "I have to stay on campus due to the nursing program. Does anyone have any spare handsanitizer? The stores have run out.",
+            tags: ["Resources", "On-Campus"],
+            datetime: "2020-02-06T22:45:28Z"
+        }
     ]
 }
