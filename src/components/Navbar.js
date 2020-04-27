6565df6f285af3cb6fd8adb123404f0b1d6f1344
Adam Del Castillo <ADelCastillo@Adams-MacBook-Pro.local>
2020-04-27 01:36:04 -0400

    Conditional NavBar rendering

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,32 +1,41 @@
 import React from 'react';
 import css from './Navbar.module.css';
-import  {Link} from 'react-router-dom';
+import  {Link, useLocation} from 'react-router-dom';
 import {HomeRounded, MessageRounded, AddRounded, PersonRounded} from '@material-ui/icons'
 
 function Navbar() {
+    
+    let location = useLocation();
+    console.log(location)
+    let hideNav = (location.pathname === '/') || (location.pathname === '/signIn')
+    console.log(hideNav)
+    
     return (
-        <nav className={css.navbar}>
-            <div className={css.navItem}>
-                <Link to='/home'>
-                    <HomeRounded fontSize='large' />
-                </Link>
-            </div>
-            <div className={css.navItem}>
-                <Link to='/responses'>
-                    <MessageRounded fontSize='large' />
-                </Link>
-            </div>
-            <div className={css.navItem}>
-                <Link to='/createNew'>
-                    <AddRounded fontSize='large' />
-                </Link>
-            </div>
-            <div className={css.navItem}>
-                <Link to='/profile'>
-                    <PersonRounded fontSize='large' />
-                </Link>
-            </div>
-        </nav>
+        <div>
+            { hideNav ? null :  
+            <nav className={css.navbar}>
+                <div className={css.navItem}>
+                    <Link to='/home'>
+                        <HomeRounded fontSize='large' />
+                    </Link>
+                </div>
+                <div className={css.navItem}>
+                    <Link to='/responses'>
+                        <MessageRounded fontSize='large' />
+                    </Link>
+                </div>
+                <div className={css.navItem}>
+                    <Link to='/createNew'>
+                        <AddRounded fontSize='large' />
+                    </Link>
+                </div>
+                <div className={css.navItem}>
+                    <Link to='/profile'>
+                        <PersonRounded fontSize='large' />
+                    </Link>
+                </div>
+         </nav>}
+        </div>
     )
 }
 
